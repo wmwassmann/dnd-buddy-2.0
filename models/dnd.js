c436f165f0bb1e4115f30770ba2e117b5a8b2063
digilau <rickyfukfuk@gmail.com>
2020-09-19 21:49:31 -0700

    amend the code from raceModel to palyer_race after debugging

--- models/dnd.js
+++
@@ -1,1 +0,0 @@
-// Import the ORM to create functions that will interact with the database.
--- models/racesModel.js
+++ models/racesModel.js
@@ -1,31 +1,33 @@
 'use strict';
 
 module.exports = function (sequelize, DataTypes) {
-    const player_race = sequelize.define('Races', {
-        race: DataTypes.STRING,
-        as_str: DataTypes.INTEGER,
-        as_dex: DataTypes.INTEGER,
-        as_con: DataTypes.INTEGER,
-        as_int: DataTypes.INTEGER,
-        as_wis: DataTypes.INTEGER,
-        as_cha: DataTypes.INTEGER,
-        language: DataTypes.STRING,
-        language2: DataTypes.STRING,
-      },
-    {
-        freezeTableName: true,
-        paranoid: true,
-    });
-    
-    racesModel.associate = function(models) {
-      // We're saying that a racesModel should belong to an Author
-      // A racesModel can't be created without an Author due to the foreign key constraint
-      racesModel.belongsTo(models.mainDatabase, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-      return player_race;
+	const player_race = sequelize.define(
+		'Races',
+		{
+			race: DataTypes.STRING,
+			as_str: DataTypes.INTEGER,
+			as_dex: DataTypes.INTEGER,
+			as_con: DataTypes.INTEGER,
+			as_int: DataTypes.INTEGER,
+			as_wis: DataTypes.INTEGER,
+			as_cha: DataTypes.INTEGER,
+			language: DataTypes.STRING,
+			language2: DataTypes.STRING,
+		},
+		{
+			freezeTableName: true,
+			paranoid: true,
+		}
+	);
 
+	player_race.associate = function (models) {
+		// We're saying that a racesModel should belong to an Author
+		// A racesModel can't be created without an Author due to the foreign key constraint
+		player_race.belongsTo(models.mainDatabase, {
+			foreignKey: {
+				allowNull: false,
+			},
+		});
+	};
+	return player_race;
 };
