11509f2d5f768c68675a42e37b8f8f31cc61e6b5
digilau <rickyfukfuk@gmail.com>
2020-09-22 14:54:17 -0700

    fixed the bug for the mistype for the front-end register.js

--- seeders/06_user-seed.js
+++ seeders/06_user-seed.js
@@ -1,6 +1,8 @@
 'use strict';
 const date = new Date();
 
+// the original login password for aa@aa.com is 11111111
+// the original login password for bb@b.com is 22222222
 const inputData = [
 	['aa@aa.com', '$2y$10$5FtL6gG2ddyUf/9pWKvebOssL4Rw.x32o51Wh/psVA9U/5NWXiQ8K'],
 	['bb@bb.com', '$2y$10$INe1vl9eyXC3Hm3gCw954eH.3m2JeOAoxOjd7mVAXwQ3yWAfHUNsK'],
