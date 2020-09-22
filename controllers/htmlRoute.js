c3b56a9f153dffdaf67d441f95aa7104af3c6c2f
digilau <rickyfukfuk@gmail.com>
2020-09-22 05:28:07 -0700

    add the register page and add the routing for the registation

--- controllers/htmlRoute.js
+++ controllers/htmlRoute.js
@@ -5,15 +5,25 @@
 // Dependencies
 // =============================================================
 const express = require('express');
-
 const router = express.Router();
 
 // Import the model (index.js) to use its database functions.
 const db = require('../models');
 
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require('../config/middleware/isAuthenticated');
+
 // Routes
 // =============================================================
 
+// the route for register the account to our database
+router.get('/register', function (req, res) {
+	// If the user already has an account send them to the members page
+	if (req.user) {
+		return res.redirect('/');
+	}
+	res.render('register', { layout: 'auth' });
+});
 // Create all our routes and set up logic within those routes where required.
 router.get('/', function (req, res) {
 	res.render('index');
