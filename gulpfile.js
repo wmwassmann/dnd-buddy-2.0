0e3009e0370eac1c58e5401ad250c067d27ca273
William Wassmann <wmwassmann@hotmail.com>
2020-09-29 13:53:31 -0700

    commiting travis stuff and things

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+1 // Development tasks
+2 // --------------------------------------------------------------------------
+3 gulp.task('travis', ['build', 'testServerJS'], function() {
+  process.exit(0);
+});
+
+ 
