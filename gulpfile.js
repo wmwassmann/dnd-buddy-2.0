7217c08fd1fec15c22e7e8e02864c48639a7c662
William Wassmann <wmwassmann@hotmail.com>
2020-10-02 10:36:12 -0700

    Working with the travis.yml and gulpfile settings.  Found a few things that might solve our build issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 1 // Development tasks
 2 // --------------------------------------------------------------------------
-3 gulp.task('travis', ['build', 'dnd-buddy-2.0'], function() {
+
+before_script:
+  - npm install -g gulp-cli
+script: gulp
+
+gulp.task('travis', ['build', 'dnd-buddy-2.0'], function() {
   process.exit(0);
 });
 
